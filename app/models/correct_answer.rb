3e4c4b8f3dc07d7153f1169700a5a0ed061ad3ac
Julius <junhyeok.dev@gmail.com>
2024-12-15 17:43:06 +0900

    :zap: 정답을 캐시해두고 사용하도록 수정

--- app/models/correct_answer.rb
+++ app/models/correct_answer.rb
@@ -1,2 +1,9 @@
 class CorrectAnswer < ApplicationRecord
+  class << self
+    def current_answer
+      Rails.cache.fetch("todays_answer", expires_at: Date.today.end_of_day) do
+        find_by(date: Date.today)
+      end
+    end
+  end
 end
